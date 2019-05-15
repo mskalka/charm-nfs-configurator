9f3f54d8da88b18c9e3921afc459773ad9ff4ea3
Michael Skalka <michael.skalka@canonical.com>
2019-05-15 09:43:55 +0200

    swapped to list for options, juju lint cleanup

--- src/reactive/nfs_configurator.py
+++ src/reactive/nfs_configurator.py
@@ -1,13 +1,14 @@
 from charms.reactive import when
-from charmhelpers.core.hookenv import config
+from charmhelpers.core.hookenv import config, status_set
 from jinja2 import Template
 
 
 @when('config.changed')
 def config_changed():
     ctxt = {}
-    ctxt['nfs_options'] = config('nfs-common-options')
+    ctxt['nfs_options'] = config('nfs-common-options').split(',')
     with open('templates/nfs-common', 'r') as t:
         nfs_template = Template(t.read())
     with open('/etc/default/nfs-common', 'w') as f:
         f.write(nfs_template.render(ctxt))
+    status_set('active', 'Unit is ready')
